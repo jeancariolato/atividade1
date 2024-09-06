e74c939572bf99fd29b38d1fcc9c4733f4d55288
jeancariolato <jlucacorrea@proton.me>
2024-09-06 10:48:51 -0300

    Update tela login

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 
 void main() {
@@ -13,68 +11,119 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        backgroundColor: Colors.black,
-        body: Center(
-          child: Column(
+      home: DefaultTabController(
+        length: 2, // Número de abas
+        child: Scaffold(
+          backgroundColor: Colors.black,
+          appBar: TabBar(
+            //ALTERAR CORES DA TAB BAR
+            indicatorWeight: 2,
+            labelColor: const Color.fromARGB(255, 214, 175, 175),
+            unselectedLabelColor: const Color.fromARGB(255, 94, 94, 94),
+            indicatorColor: const Color.fromARGB(255, 255, 255, 255),
+            tabs: [
+              Tab(text: "Login"),
+              Tab(text: "Cadastro"),
+            ],
+          ),
+          body: TabBarView(
             children: [
-              //CARREGAR IMAGEM
-              Container(
-                width: 200,
-                height: 300,
-                child: Image.network(
-                    'https://i.pinimg.com/736x/ba/3a/1e/ba3a1ebe3532604307b99931d34b4a27.jpg'),
-              ),
+              // Primeira aba - Conteúdo Login
+              Center(
+                child: Column(
+                  children: [
+                    //CARREGAR IMAGEM
+                    SizedBox(height: 35),
+                    Container(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment
+                            .start, // Alinha o texto à esquerda
+                        children: [
+                          Image.network(
+                            "https://i.pinimg.com/736x/f7/75/23/f7752374579381577cc4f89c28f0b134.jpg",
+                            width: double
+                                .infinity, // Faz a imagem ocupar a largura máxima
+                            fit: BoxFit
+                                .cover, // Ajusta a imagem para cobrir o espaço
+                          ),
+                          SizedBox(
+                              height: 10), // Espaço entre a imagem e o texto
+                          Text(
+                            "Bem-vindo ao App Atividade1!",
+                            style: TextStyle(color: Colors.white, fontSize: 35),
+                            textAlign:
+                                TextAlign.left, // Alinha o texto à esquerda
+                          ),
+                        ],
+                      ),
+                    ),
 
-              //CAMPO DE TEXTO 1
-              Container(
-                width: 300,
-                height: 40,
-                child: TextField(
-                  decoration: const InputDecoration(
-                      focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(
-                            color: Colors.blue), // Cor da borda quando focado
+                    SizedBox(height: 35),
+                    //CAMPO DE TEXTO 1
+                    Container(
+                      width: 300,
+                      height: 40,
+                      child: TextField(
+                        decoration: const InputDecoration(
+                            prefixIcon: Icon(Icons.people_alt),
+                            focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                  color: Color.fromARGB(255, 255, 255,
+                                      255)), // Cor da borda quando focado
+                            ),
+                            labelText: "Usuário"),
                       ),
-                      labelText: "Usuário"),
+                    ),
+                    SizedBox(height: 35),
+                    //CAMPO DE TEXTO 2
+                    Container(
+                      width: 300,
+                      height: 40,
+                      child: TextField(
+                        decoration: const InputDecoration(
+                            prefixIcon: Icon(Icons.lock),
+                            focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                  color: Color.fromARGB(255, 255, 255,
+                                      255)), // Cor da borda quando focado
+                            ),
+                            labelText: "Senha"),
+                      ),
+                    ),
+                    SizedBox(height: 35),
+                    //BOTAO PRETO COM TEXTO BRANCO 'LOGIN'
+                    ElevatedButton(
+                        style: ButtonStyle(
+                          shape: WidgetStatePropertyAll(
+                            RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(8.0)),
+                          ),
+                          backgroundColor: WidgetStatePropertyAll(
+                              const Color.fromARGB(255, 255, 255, 255)),
+                          foregroundColor: WidgetStatePropertyAll(
+                              const Color.fromARGB(255, 0, 0, 0)),
+                        ),
+                        onPressed: () {},
+                        child: Padding(
+                          padding: const EdgeInsets.fromLTRB(
+                              110.0, 15.0, 110.0, 15.0),
+                          child: Text(
+                            "Login",
+                            style: TextStyle(fontSize: 16),
+                          ),
+                        )),
+                  ],
                 ),
               ),
-              SizedBox(height: 35),
-              //CAMPO DE TEXTO 2
-              Container(
-                width: 300,
-                height: 40,
-                child: TextField(
-                  decoration: const InputDecoration(
-                      focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(
-                            color: Colors.blue), // Cor da borda quando focado
-                      ),
-                      labelText: "Senha"),
+              // Segunda aba - Pode usar o mesmo conteúdo ou personalizar
+              Center(
+                child: Column(
+                  children: [
+                    Text('Página de Cadastro'),
+                    // Adicione outros campos de cadastro aqui, se desejar
+                  ],
                 ),
               ),
-              SizedBox(height: 35),
-              //BOTAO PRETO COM TEXTO BRANCO 'LOGIN'
-              ElevatedButton(
-                  style: ButtonStyle(
-                    shape: WidgetStatePropertyAll(
-                      RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(8.0)),
-                    ),
-                    backgroundColor: WidgetStatePropertyAll(
-                        const Color.fromARGB(255, 255, 255, 255)),
-                    foregroundColor: WidgetStatePropertyAll(
-                        const Color.fromARGB(255, 0, 0, 0)),
-                  ),
-                  onPressed: () {},
-                  child: Padding(
-                    padding:
-                        const EdgeInsets.fromLTRB(110.0, 15.0, 110.0, 15.0),
-                    child: Text(
-                      "Login",
-                      style: TextStyle(fontSize: 16),
-                    ),
-                  )),
             ],
           ),
         ),
